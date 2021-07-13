d10a431cc137d1cc446578397efda5df2c8a9ba4
Daniel Zamanillo <dzamanillok@gmail.com>
2021-07-13 16:10:51 -0400

    save and load functions built but not working properly

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,3 +1,4 @@
+// *Time Coding Functions
 var getTime = function () {
 	var time = moment();
 	time = time.format("k");
@@ -26,3 +27,78 @@ var getTime = function () {
 };
 getTime();
 var updateTime = setInterval(getTime, 3000);
+
+// * Local Storage Array
+var tasks = [
+	{
+		time: "nine-am-text",
+		text: "",
+	},
+	{
+		time: "ten-am-text",
+		text: "",
+	},
+	{
+		time: "eleven-am-text",
+		text: "",
+	},
+	{
+		time: "twelve-pm-text",
+		text: "",
+	},
+	{
+		time: "one-pm-text",
+		text: "",
+	},
+	{
+		time: "two-pm-text",
+		text: "",
+	},
+	{
+		time: "three-pm-text",
+		text: "",
+	},
+	{
+		time: "four-pm-text",
+		text: "",
+	},
+	{
+		time: "five-pm-text",
+		text: "",
+	},
+];
+
+//* Save Tasks Function
+var saveTasks = function () {
+	localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+//TODO Load Tasks Function
+var loadTasks = function () {
+	var savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+	console.log(typeof savedTasks);
+
+	for (var i = 0; i < savedTasks.length; i++) {}
+};
+loadTasks();
+
+// * Save Button
+$(".container").on("click", "button", function (event) {
+	event.preventDefault();
+	loadTasks();
+	// Target row of button
+	var row = event.target.closest(".row");
+	// Target value of textarea
+	var newText = row.childNodes[3].value;
+	// Set textarea value to new text
+	row.childNodes[3].textContent = newText;
+
+	// Get data-time
+	var dataTimeValue = row.childNodes[3].getAttribute("data-time");
+	// Turn to int
+	dataTimeValue = parseInt(dataTimeValue);
+
+	tasks[dataTimeValue - 9].text = newText;
+	saveTasks();
+});
